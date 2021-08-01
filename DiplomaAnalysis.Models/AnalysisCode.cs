07f0704b39deda59564df3ad27e0fab6a4039c8c
Yakiv Yusin <yusin.yakiv@gmail.com>
2021-08-01 17:10:08 +0300

    Added Layout Analysis Service

--- DiplomaAnalysis.Models/AnalysisCode.cs
+++ DiplomaAnalysis.Models/AnalysisCode.cs
@@ -3,5 +3,7 @@
     public static class AnalysisCode
     {
         public const string References = "REF01";
+        public const string PageSize = "LAY01";
+        public const string PageMargin = "LAY02";
     }
 }
---
+++ DiplomaAnalysis.Services.Layout/LayoutService.cs
@@ -0,0 +1,82 @@
+﻿using DiplomaAnalysis.Models;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DiplomaAnalysis.Services.Layout
+{
+    public class LayoutService : IDisposable
+    {
+        private static readonly int Width = (int)Math.Round(21.0 * 1440 / 2.54);
+        private static readonly int Height = (int)Math.Round(29.7 * 1440 / 2.54);
+        private static readonly int LeftMargin = (int)Math.Round(3.0 * 1440 / 2.54);
+        private static readonly int OtherMargins = (int)Math.Round(2.0 * 1440 / 2.54);
+        private static readonly int AllowedDifference = 10;
+
+        private readonly WordprocessingDocument _document;
+
+        public LayoutService(Stream data)
+        {
+            _document = WordprocessingDocument.Open(data, false);
+        }
+
+        public IEnumerable<MessageDto> Analyze()
+        {
+            var sectionProperties = _document
+                .MainDocumentPart
+                .Document
+                .Body
+                .Descendants<SectionProperties>()
+                .ToList();
+
+            if (sectionProperties
+                .Select(x => x.GetFirstChild<PageSize>())
+                .Any(x => !IsPageSizeCorrect(x)))
+            {
+                yield return new MessageDto
+                {
+                    Code = AnalysisCode.PageSize,
+                    IsError = true,
+                    ExtraMessage = null
+                };
+            }
+
+            if (sectionProperties
+                .Select(x => x.GetFirstChild<PageMargin>())
+                .Any(x => !IsPageMarginCorrect(x)))
+            {
+                yield return new MessageDto
+                {
+                    Code = AnalysisCode.PageMargin,
+                    IsError = false,
+                    ExtraMessage = null
+                };
+            }
+        }
+
+        private bool IsPageSizeCorrect(PageSize size)
+        {
+            return
+                Math.Abs(size.Width - Width) <= AllowedDifference &&
+                Math.Abs(size.Height - Height) <= AllowedDifference;
+        }
+
+        private bool IsPageMarginCorrect(PageMargin margin)
+        {
+            return
+                Math.Abs(margin.Left - LeftMargin) <= AllowedDifference &&
+                Math.Abs(margin.Right - OtherMargins) <= AllowedDifference &&
+                Math.Abs(margin.Top - OtherMargins) <= AllowedDifference &&
+                Math.Abs(margin.Bottom - OtherMargins) <= AllowedDifference;
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)_document).Dispose();
+        }
+    }
+}
