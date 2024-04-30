3e31f8daae7545457531f233cc462bfe2e7eb315
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-04-30 15:55:16 +0300

    Change Error Code for Runglish

--- DiplomaAnalysis.Common.Models/AnalysisCode.cs
+++ DiplomaAnalysis.Common.Models/AnalysisCode.cs
@@ -7,11 +7,11 @@ public static class AnalysisCode
     public const string PageSize = "LAY01";
     public const string PageMargin = "LAY02";
     public const string Orthography2019 = "ORT01";
-    public const string Runglish = "ORT02";
     public const string WordingMisuse = "ORT03";
     public const string CharacterReplacement = "ORT04";
     public const string PunctuationSpacing = "PNC01";
     public const string Quotes = "PNC02";
     public const string Typographical = "PNC03";
+    public const string Runglish = "STL01";
     public const string Pronouns = "STL02";
 }
